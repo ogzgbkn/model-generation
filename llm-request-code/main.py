76488f6ba0420ad1df1ced86bcd1b2cffbf1f504
Oguz Gögebakan <ogzgbkn@w207-4c-v4.eduroam.dynamic.rbg.tum.de>
2024-12-18 15:55:08 +0100

    Game files and prompts are added.

--- llm-request-code/main.py
+++ llm-request-code/main.py
@@ -5,13 +5,15 @@ from dotenv import dotenv_values
 from experiments import workflow_defs
 from req_smell_tracing.workflow import Workflow
 
+from pathlib import Path
+
 config = dotenv_values(".env")
 logger = logging.getLogger(__name__)
 
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
-
+    root = Path(__file__).resolve()
     workflow = Workflow.load_workflow(
         config, workflow_defs=[workflow_defs.def_workflow]
     )

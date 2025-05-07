b957090a57f3b2f7ad4974f286d1f2c152e36c46
Oguz Gögebakan <ogzgbkn@w211-64-v4.eduroam.dynamic.rbg.tum.de>
2025-05-07 20:16:44 +0200

    Critical and non critical reasons percentages for non-smelly variants added.

--- evaluation_analysis/helpers.py
+++ evaluation_analysis/helpers.py
@@ -1,3 +1,8 @@
+import os
+import csv
+import math
+import copy
+
 def get_num_of_requirements(game_name):
     games = {
         'dice_game': 25,
@@ -7,3 +12,122 @@ def get_num_of_requirements(game_name):
         'pong': 20
     }
     return games[game_name]
+
+
+class Reason:
+    def __init__(self):
+        self.non_critical_completeness_reasons = ['1.3.1', '1.4.1', '1.5.1', '1.6', '1.6.1']
+        self.non_critical_correctness_reasons = ['2.3.1', '2.3.2', '2.4.1', '2.5.1', '2.6.1']
+        self.reason_counts = {
+            'dice_game': {},
+            'arkanoid': {},
+            'snake': {},
+            'scopa': {},
+            'pong': {}
+        }
+
+        current_file_path = os.path.abspath(__file__)
+        # Directory containing the file (./model-generation/evaluation_analysis)
+        current_dir = os.path.dirname(current_file_path)
+        # Path to your CSV file
+        csv_file_path = os.path.join(current_dir, 'games_component_counts.csv')
+
+        # Open the CSV file
+        with open(csv_file_path, mode='r', newline='', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            # If the CSV has a header and you want to skip it
+            header = next(reader, None)
+            # Process each row
+            game = None
+            for row_number, row in enumerate(reader, start=1):
+                for col_index, value in enumerate(row):
+                    if row[0] in self.reason_counts:
+                        game = row[0]
+                        if col_index != 0:
+                            reasons = header[col_index]
+                            reasons_list = reasons.split(',')
+                            for reason in reasons_list:
+                                self.reason_counts[game][reason] = int(value)
+
+    def calc_critical_and_non_critical_percentages(self, evals_info, games = [], variants = []):
+        inner_dict = {
+            'critical_reasons': {},
+            'non_critical_reasons': {},
+        }
+        counts_dict = {
+            'dice_game': copy.deepcopy(inner_dict),
+            'arkanoid': copy.deepcopy(inner_dict),
+            'snake': copy.deepcopy(inner_dict),
+            'scopa': copy.deepcopy(inner_dict),
+            'pong': copy.deepcopy(inner_dict)
+        }
+        reason_types = ['completeness_reasons', 'correctness_reasons']
+        # Next 3 for loops goes through all evaluations
+        # Finding counts of critical and non-critical reasons by games
+        for game, results in evals_info.items():
+            if games and game not in games:
+                    continue
+            for variant_name, variant_results in results.items():
+                if variants and variant_name not in variants:
+                    continue
+                for req_id_str, req_result in variant_results.items():
+                    for reason_type in reason_types:
+                        if reason_type in req_result and req_result[reason_type]:
+                            for reason in req_result[reason_type]:
+                                if reason_type == 'completeness_reasons':
+                                    non_critical_list = self.non_critical_completeness_reasons
+                                else:
+                                    non_critical_list = self.non_critical_correctness_reasons
+                                if reason in non_critical_list:
+                                    critical_type = 'non_critical_reasons'
+                                else:
+                                    critical_type = 'critical_reasons'
+                                # 2.7 can be critical or non critical depending on the completeness reasons
+                                if 'correctness_reasons' in req_result and req_result['correctness_reasons'] == ['2.7']:
+                                    if 'completeness_reasons' in req_result and req_result['completeness_reasons']:
+                                        completeness_reasons_set = set(req_result['completeness_reasons'])
+                                        if completeness_reasons_set.issubset(set(self.non_critical_completeness_reasons)):
+                                            critical_type = 'non_critical_reasons'
+                                counts_dict[game][critical_type][reason] = counts_dict[game][critical_type][reason] + 1 if reason in counts_dict[game][critical_type] else 1
+
+        inner_percentage_dict = {
+            'critical_reasons_percentage': 0,
+            'non_critical_reasons_percentage': 0,
+        }
+        percentages_dict = {
+            'dice_game': copy.deepcopy(inner_percentage_dict),
+            'arkanoid': copy.deepcopy(inner_percentage_dict),
+            'snake': copy.deepcopy(inner_percentage_dict),
+            'scopa': copy.deepcopy(inner_percentage_dict),
+            'pong': copy.deepcopy(inner_percentage_dict)
+        }
+        critical_types = ['critical_reasons', 'non_critical_reasons']
+        
+        # Normalization and summation of counts
+        for game, percentages in percentages_dict.items():
+            for critical_type in critical_types:
+                reason_counts = counts_dict[game][critical_type]
+                total_normalized_reasons_count = 0
+                if reason_counts:
+                    for reason, count in reason_counts.items():
+                        reason_prefix = reason[0:3]
+                        normalized_reason_count = count / self.reason_counts[game][reason_prefix]
+                        total_normalized_reasons_count += normalized_reason_count
+                percentages[f"{critical_type}_percentage"] = total_normalized_reasons_count
+            
+            sum = percentages_dict[game]['critical_reasons_percentage'] + percentages_dict[game]['non_critical_reasons_percentage']
+            percentages_dict[game]['critical_reasons_percentage'] = (percentages_dict[game]['critical_reasons_percentage'] / sum) * 100
+            percentages_dict[game]['non_critical_reasons_percentage'] = (percentages_dict[game]['non_critical_reasons_percentage'] / sum) * 100
+
+        total_critical_reasons_percentage = 0
+        total_non_critical_reasons_percentage = 0
+        for game, percentages in percentages_dict.items():
+            total_critical_reasons_percentage += percentages['critical_reasons_percentage']
+            total_non_critical_reasons_percentage += percentages['non_critical_reasons_percentage']
+
+        percentages_dict['average'] = {
+            'critical_reasons_percentage': total_critical_reasons_percentage / 5,
+            'non_critical_reasons_percentage': total_non_critical_reasons_percentage / 5,
+        }
+
+        return percentages_dict
--- evaluation_analysis/rq1_diagram_generators.py
+++ evaluation_analysis/rq1_diagram_generators.py
@@ -3,16 +3,15 @@ import matplotlib.pyplot as plt
 import csv
 import numpy as np
 
-from helpers import get_num_of_requirements
+from helpers import get_num_of_requirements, Reason
 from general_diagram_generators import create_reason_counts, calc_normalized_reason_counts_general, plot_top_10_reasons
 
 
 def generate_rq1_diagrams(evals_info):
     create_non_smelly_score_percentages(evals_info)
     create_reason_counts(evals_info, calc_normalized_reason_counts_no_smells, context = 'Non-smelly Variants Only', requested_variants = ['no_smells'], directory = 'rq1')
-    create_non_smelly_score_percentages_both_comp_corr_true(evals_info)
-    # Also creates tolerated reason counts and tolerated comp and corr true
-    create_non_smelly_score_percentages_tolerated(evals_info)
+    create_non_smelly_score_percentages_both_comp_corr_true(evals_info, directory = 'rq1', file_name = 'completeness_correctness_percentages_both_true.png', context = 'Non-smelly Variants Only')
+    create_non_smelly_critical_and_non_critical_reasons_percentage(evals_info)
 
 
 def create_non_smelly_score_percentages(evals_info):
@@ -84,7 +83,7 @@ def create_non_smelly_score_percentages(evals_info):
     plt.savefig("evaluation_analysis/diagrams/rq1/completeness_correctness_percentages.png", dpi=300, bbox_inches='tight')
 
 
-def create_non_smelly_score_percentages_both_comp_corr_true(evals_info):
+def create_non_smelly_score_percentages_both_comp_corr_true(evals_info, directory = '', file_name = '', context = None):
     percentages_dict = {}
     total_comp_corr_percentage = 0
     for game, results in evals_info.items():
@@ -124,7 +123,10 @@ def create_non_smelly_score_percentages_both_comp_corr_true(evals_info):
 
     # Labels and title
     ax.set_ylabel('Percentage')
-    chart_title = 'Comp. and Corr. Both True Percentages (Non-Smelly Variants Only)'
+    if context:
+        chart_title = f'Comp. and Corr. Both True Percentages ({context})'
+    else:
+        chart_title = 'Comp. and Corr. Both True Percentages (All Evaluations)'
     ax.set_title(chart_title)
 
     # Correctly center tick labels under each group
@@ -140,126 +142,22 @@ def create_non_smelly_score_percentages_both_comp_corr_true(evals_info):
 
     plt.tight_layout()
     
-    os.makedirs("evaluation_analysis/diagrams/rq1", exist_ok=True)
-    plt.savefig("evaluation_analysis/diagrams/rq1/completeness_correctness_percentages_both_true.png", dpi=300, bbox_inches='tight')
+    os.makedirs(f"evaluation_analysis/diagrams/{directory}", exist_ok=True)
+    plt.savefig(f"evaluation_analysis/diagrams/{directory}/{file_name}", dpi=300, bbox_inches='tight')
 
 
 def calc_normalized_reason_counts_no_smells(reasons_dict):
     return calc_normalized_reason_counts_general(reasons_dict, all_evals = False)
 
 
-def create_non_smelly_score_percentages_tolerated(evals_info):
-    comp_corr_one_percentages_dict = {}
-    total_comp_corr_one_percentage = 0
-
-    percentages_dict = {}
-    reasons_dict = {
-        'completeness_reasons': {},
-        'correctness_reasons': {},
-    }
-
-    tolerated_completeness_reasons = ['1.3', '1.3.1', '1.4.1', '1.5.1', '1.6', '1.6.1']
-    tolerated_completeness_reasons_set = set(tolerated_completeness_reasons)
-    tolerated_correctness_reasons = ['2.3', '2.3.1', '2.3.2', '2.3.3', '2.4.1', '2.5.1', '2.6', '2.6.1']
-    tolerated_correctness_reasons_set = set(tolerated_correctness_reasons)
-
-    total_completeness_percentage = 0
-    total_correctness_percentage = 0
-    for game, results in evals_info.items():
-        total_comp_tol_count = 0
-        total_corr_tol_count = 0
-        game_req_count = get_num_of_requirements(game)
-        game_tolerated_comp_total = 0
-        game_tolerated_corr_total = 0
-        game_no_smells_total_comp_corr_one = 0
-        # Checking each no-smells variant
-        for req_id_str, req_result in results['no_smells'].items():
-            if req_id_str != 'total':
-                tolerate_completeness = False
-                tolerate_correctness = False
-                
-                # Toleration of less impactful smells
-                eval_completeness_reasons_set = set(req_result['completeness_reasons'])
-                eval_correctness_reasons_set = set(req_result['correctness_reasons'])
-                # The sets should not be empty
-                if eval_completeness_reasons_set and eval_completeness_reasons_set.issubset(tolerated_completeness_reasons_set):
-                    tolerate_completeness = True
-                if eval_correctness_reasons_set and eval_correctness_reasons_set.issubset(tolerated_correctness_reasons_set):
-                    tolerate_correctness = True
-                # If completeness can be tolerated and correctness only has 2.7, correctness can also be tolerated
-                elif req_result['correctness_reasons'] == ['2.7'] and tolerate_completeness:
-                    tolerate_correctness = True
-
-                if tolerate_completeness:
-                    total_comp_tol_count = total_comp_tol_count + 1
-                if tolerate_correctness:
-                    total_corr_tol_count = total_corr_tol_count + 1
-
-                # For calculating the tolerated no smelly reason percentages
-                reason_types = ['completeness_reasons', 'correctness_reasons']
-                for reason_type in reason_types:
-                    # Skipping the tolerated reasons
-                    if (reason_type == 'completeness_reasons' and tolerate_completeness) or (reason_type == 'correctness_reasons' and tolerate_correctness):
-                        continue
-                    if reason_type in req_result and req_result[reason_type]:
-                        for reason in req_result[reason_type]:
-                            if reason in reasons_dict[reason_type]:
-                                reasons_dict[reason_type][reason] = reasons_dict[reason_type][reason] + 1
-                            else:
-                                reasons_dict[reason_type][reason] = 1
-
-                completeness_to_aggregate = 1 if tolerate_completeness else req_result['completeness']
-                correctness_to_aggregate = 1 if tolerate_correctness else req_result['correctness']
-
-                # For generating comp corr one tolerated percentages
-                if completeness_to_aggregate == 1 and correctness_to_aggregate == 1:
-                    game_no_smells_total_comp_corr_one = game_no_smells_total_comp_corr_one + 1
-                
-                game_tolerated_comp_total = game_tolerated_comp_total + completeness_to_aggregate
-                game_tolerated_corr_total = game_tolerated_corr_total + correctness_to_aggregate
-
-        print(f"Game: {game}, comp tol count: {total_comp_tol_count}, corr tol count: {total_corr_tol_count}")
-        
-        # For generating comp corr one tolerated percentages
-        comp_corr_one_percentage = (int(game_no_smells_total_comp_corr_one) / game_req_count) * 100
-        comp_corr_one_percentages_dict[game] = {
-            'comp_corr_percentage': comp_corr_one_percentage,
-        }
-        total_comp_corr_one_percentage = total_comp_corr_one_percentage + comp_corr_one_percentage
-        
-        completeness_percentage = (int(game_tolerated_comp_total) / game_req_count) * 100
-        correctness_percentage = (int(game_tolerated_corr_total) / game_req_count) * 100
-        percentages_dict[game] = {
-            'completeness_percentage': completeness_percentage,
-            'correctness_percentage': correctness_percentage,
-        }
-        total_completeness_percentage = total_completeness_percentage + completeness_percentage
-        total_correctness_percentage = total_correctness_percentage + correctness_percentage
-    
-    avg_comp_corr = total_comp_corr_one_percentage / 5
-    comp_corr_one_percentages_dict['average'] = {
-        'comp_corr_percentage': avg_comp_corr
-    }
-
-    create_non_smelly_tolerated_comp_corr_one(comp_corr_one_percentages_dict)
-    
-    avg_completeness = total_completeness_percentage / 5
-    avg_correctness = total_correctness_percentage / 5
-    percentages_dict['average'] = {
-        'completeness_percentage': avg_completeness,
-        'correctness_percentage': avg_correctness,
-    }
-
-    normalized_reasons_dict = calc_normalized_reason_counts_general(reasons_dict, all_evals = False)
-
-    # Plot and save diagrams
-    plot_top_10_reasons(normalized_reasons_dict['completeness_reasons'], "Top 10 Completeness Reasons (Non-smelly Variants Only)", "completeness_reasons_tolerated.png", 'rq1')
-    plot_top_10_reasons(normalized_reasons_dict['correctness_reasons'], "Top 10 Correctness Reasons (Non-smelly Variants Only)", "correctness_reasons_tolerated.png", 'rq1')
+def create_non_smelly_critical_and_non_critical_reasons_percentage(evals_info):
+    reason = Reason()
+    reasons_percentages = reason.calc_critical_and_non_critical_percentages(evals_info, variants = ['no_smells'])
 
     # Ordered keys
     ordered_keys = ['average', 'dice_game', 'arkanoid', 'snake', 'scopa', 'pong']
-    completeness = [percentages_dict[k]["completeness_percentage"] for k in ordered_keys]
-    correctness = [percentages_dict[k]["correctness_percentage"] for k in ordered_keys]
+    critical_reasons_percentages = [reasons_percentages[k]["critical_reasons_percentage"] for k in ordered_keys]
+    non_critical_reasons_percentages = [reasons_percentages[k]["non_critical_reasons_percentage"] for k in ordered_keys]
 
     # Plot config
     bar_width = 0.35
@@ -273,12 +171,12 @@ def create_non_smelly_score_percentages_tolerated(evals_info):
     fig, ax = plt.subplots(figsize=(10, 6))
 
     # Plot bars
-    ax.bar(group_centers - bar_width / 2, completeness, width=bar_width, color='blue', label='Completeness')
-    ax.bar(group_centers + bar_width / 2, correctness, width=bar_width, color='red', label='Correctness')
+    ax.bar(group_centers - bar_width / 2, critical_reasons_percentages, width=bar_width, color='red', label='Critical')
+    ax.bar(group_centers + bar_width / 2, non_critical_reasons_percentages, width=bar_width, color='green', label='Non-critical')
 
     # Labels and title
     ax.set_ylabel('Percentage')
-    ax.set_title('Completeness vs. Correctness Percentages (Non-smelly Variants Only)')
+    ax.set_title('Critical vs. Non-Critical Reasons Percentages (Non-smelly Variants Only)')
 
     # Correctly center tick labels under each group
     ax.set_xticks(group_centers)
@@ -298,43 +196,4 @@ def create_non_smelly_score_percentages_tolerated(evals_info):
     plt.tight_layout()
     
     os.makedirs("evaluation_analysis/diagrams/rq1", exist_ok=True)
-    plt.savefig("evaluation_analysis/diagrams/rq1/completeness_correctness_percentages_tolerated.png", dpi=300, bbox_inches='tight')
-
-
-def create_non_smelly_tolerated_comp_corr_one(percentages_dict):
-    # Ordered keys
-    ordered_keys = ['average', 'dice_game', 'arkanoid', 'snake', 'scopa', 'pong']
-    comp_and_corr = [percentages_dict[k]["comp_corr_percentage"] for k in ordered_keys]
-
-    # Plot config
-    bar_width = 0.6
-    group_spacing = 0.4
-    num_groups = len(ordered_keys)
-    x_positions = np.arange(num_groups)
-
-    # Create plot
-    fig, ax = plt.subplots(figsize=(6, 6))
-
-    # Plot bars
-    ax.bar(x_positions, comp_and_corr, width=bar_width, color='red', label='Completeness and Correctness')
-
-    # Labels and title
-    ax.set_ylabel('Percentage')
-    chart_title = 'Comp. and Corr. Both True Percentages (Non-Smelly Variants Only)'
-    ax.set_title(chart_title)
-
-    # Correctly center tick labels under each group
-    ax.set_xticks(x_positions)
-    ax.set_xticklabels(ordered_keys, rotation=45, ha='center')  # <== ha='center' is key here
-
-    ax.set_ylim(0, 100)
-    ax.set_yticks(np.arange(0, 101, 10))
-    ax.legend()
-
-    # Optional: Horizontal grid
-    ax.yaxis.grid(True, linestyle='--', linewidth=0.5)
-
-    plt.tight_layout()
-    
-    os.makedirs("evaluation_analysis/diagrams/rq1", exist_ok=True)
-    plt.savefig("evaluation_analysis/diagrams/rq1/completeness_correctness_percentages_tolerated_both_true.png", dpi=300, bbox_inches='tight')
+    plt.savefig("evaluation_analysis/diagrams/rq1/critical_non_critical_reasons_percentages.png", dpi=300, bbox_inches='tight')
